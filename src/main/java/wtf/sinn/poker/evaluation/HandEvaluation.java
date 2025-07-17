690092192e313a37ea679d9bcb3851a1c3c62093
Morathii <morathii@icloud.com>
2025-07-17 21:21:38 +0200

    :recycle: Improve brevity of code by extracting methods

--- src/main/java/wtf/sinn/poker/evaluation/HandEvaluation.java
+++ src/main/java/wtf/sinn/poker/evaluation/HandEvaluation.java
@@ -7,8 +7,20 @@ import java.util.List;
 public class HandEvaluation {
     // TODO Change return type to something actually representing an evaluation result
     public boolean evaluate(List<Card> hand) {
+        return isStraightFlush(hand);
+    }
+
+    private boolean isStraightFlush(List<Card> hand) {
+        return hasOnlyCardsOfSameSuit(hand) && isUniqueAndSequential(hand);
+    }
+
+    private static boolean hasOnlyCardsOfSameSuit(List<Card> hand) {
         final var suitAtHand = hand.getFirst().cardSuit();
 
+        return hand.stream().allMatch(it -> it.cardSuit().equals(suitAtHand));
+    }
+
+    private boolean isUniqueAndSequential(List<Card> hand) {
         final var sortedValues = hand.stream()
                 .map(it -> it.cardValue().getValue())
                 .sorted()
@@ -17,10 +29,9 @@ public class HandEvaluation {
         int min = sortedValues.getFirst();
         int max = sortedValues.getLast();
 
-        // Check for uniqueness and strict consecutiveness
         boolean allUnique = sortedValues.stream().distinct().count() == sortedValues.size();
         boolean isSequential = (max - min + 1) == sortedValues.size();
 
-        return allUnique && isSequential && hand.stream().allMatch(it -> it.cardSuit().equals(suitAtHand));
+        return allUnique && isSequential;
     }
 }
