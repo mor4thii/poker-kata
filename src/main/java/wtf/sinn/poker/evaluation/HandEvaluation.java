c21943f90938ecd9f4a573ada0d3ffe7ab428624
Morathii <morathii@icloud.com>
2025-07-17 21:05:08 +0200

    :white_check_mark: Make heart straight flush check pass

--- src/main/java/wtf/sinn/poker/evaluation/HandEvaluation.java
+++ src/main/java/wtf/sinn/poker/evaluation/HandEvaluation.java
@@ -7,6 +7,8 @@ import java.util.List;
 public class HandEvaluation {
     // TODO Change return type to something actually representing an evaluation result
     public boolean evaluate(List<Card> hand) {
-        return false;
+        final var suitAtHand = hand.getFirst().cardSuit();
+
+        return hand.stream().allMatch(it -> it.cardSuit().equals(suitAtHand));
     }
 }
