86593977811fc7fbff001d46f09a7f3d4ce54ae4
Morathii <morathii@icloud.com>
2025-07-17 20:10:18 +0200

    :white_check_mark: Add a trivial test for Card creation

---
+++ src/test/java/wtf/sinn/model/CardTest.java
@@ -0,0 +1,18 @@
+package wtf.sinn.model;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+public class CardTest {
+    @Nested
+    class The_Card {
+        @Test
+        void should_build() {
+            final var actual = new Card(CardSuit.CLUBS, CardValue.TWO);
+
+            then(actual).isNotNull();
+        }
+    }
+}
