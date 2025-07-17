8c75ad05be76d1d3bdbd4f60ad9c21b82eeb83c0
Morathii <morathii@icloud.com>
2025-07-17 20:59:27 +0200

    :test_tube: Add failing test for heart straight flush

---
+++ src/main/java/wtf/sinn/poker/evaluation/HandEvaluation.java
@@ -0,0 +1,12 @@
+package wtf.sinn.poker.evaluation;
+
+import wtf.sinn.poker.model.Card;
+
+import java.util.List;
+
+public class HandEvaluation {
+    // TODO Change return type to something actually representing an evaluation result
+    public boolean evaluate(List<Card> hand) {
+        return false;
+    }
+}
---
+++ src/test/java/wtf/sinn/poker/evaluation/HandEvaluationTest.java
@@ -0,0 +1,40 @@
+package wtf.sinn.poker.evaluation;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import wtf.sinn.poker.model.Card;
+import wtf.sinn.poker.model.CardSuit;
+import wtf.sinn.poker.model.CardValue;
+
+import java.util.List;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+class HandEvaluationTest {
+
+    private HandEvaluation handEvaluation;
+
+    @BeforeEach
+    void setUp() {
+        handEvaluation = new HandEvaluation();
+    }
+
+    @Nested
+    class The_HandEvaluation {
+        @Test
+        void should_detect_a_hearts_straight_flush() {
+            final var hand = List.of(
+                    new Card(CardSuit.HEARTS, CardValue.TWO),
+                    new Card(CardSuit.HEARTS, CardValue.THREE),
+                    new Card(CardSuit.HEARTS, CardValue.FOUR),
+                    new Card(CardSuit.HEARTS, CardValue.FIVE),
+                    new Card(CardSuit.HEARTS, CardValue.SIX)
+            );
+
+            final var actual = handEvaluation.evaluate(hand);
+
+            then(actual).isTrue();
+        }
+    }
+}
