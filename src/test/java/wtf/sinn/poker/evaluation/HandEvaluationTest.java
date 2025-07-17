39f6ee0a89b8a68305e6daff00943078532eb8ab
Morathii <morathii@icloud.com>
2025-07-17 21:45:11 +0200

    :test_tube: Add a test case for four-of-a-kind cheaters that have the same suite twice

--- src/test/java/wtf/sinn/poker/evaluation/HandEvaluationTest.java
+++ src/test/java/wtf/sinn/poker/evaluation/HandEvaluationTest.java
@@ -70,5 +70,20 @@ class HandEvaluationTest {
 
             then(actual).isTrue();
         }
+
+        @Test
+        void should_detect_four_of_a_kind_cheaters() {
+            final var hand = List.of(
+                    new Card(CardSuit.HEARTS, CardValue.TWO),
+                    new Card(CardSuit.HEARTS, CardValue.TWO),
+                    new Card(CardSuit.SPADES, CardValue.TWO),
+                    new Card(CardSuit.DIAMONDS, CardValue.TWO),
+                    new Card(CardSuit.HEARTS, CardValue.THREE)
+            );
+
+            final var actual = handEvaluation.evaluate(hand);
+
+            then(actual).isFalse();
+        }
     }
 }
