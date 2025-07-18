43e075711aa617c99d3c34f94a88f60cc9d185f3
Morathii <morathii@icloud.com>
2025-07-18 20:18:25 +0200

    :bug: HandEvaluation has to be public to be of any use to the outside

--- src/main/java/wtf/sinn/poker/ranking/HandEvaluation.java
+++ src/main/java/wtf/sinn/poker/ranking/HandEvaluation.java
@@ -3,7 +3,7 @@ package wtf.sinn.poker.ranking;
 import wtf.sinn.poker.model.Hand;
 import wtf.sinn.poker.model.HandRank;
 
-final class HandEvaluation {
+public final class HandEvaluation {
     // TODO: Replace by builder
     static final HandRanker RANKING_CHAIN = new StraightFlushRanker(new FourOfAKindRanker(new FullHouseRanker(new FlushRanker(new StraightRanker(new ThreeOfAKindRanker(new TwoPairsRanker(new PairRanker(new HighCardRanker(null)))))))));
 
