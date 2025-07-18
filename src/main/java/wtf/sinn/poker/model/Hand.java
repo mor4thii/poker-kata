ed61fda5b62f49b375517c2ab90254339a3a4132
Morathii <morathii@icloud.com>
2025-07-18 17:27:07 +0200

    :white_check_mark: Implement hand invariant to have 5 cards, always

--- src/main/java/wtf/sinn/poker/model/Hand.java
+++ src/main/java/wtf/sinn/poker/model/Hand.java
@@ -3,4 +3,9 @@ package wtf.sinn.poker.model;
 import java.util.List;
 
 public record Hand(List<Card> cards) {
+    public Hand {
+        if (cards.size() != 5) {
+            throw new IllegalArgumentException("A hand must consist of 5 cards");
+        }
+    }
 }
--- src/test/java/wtf/sinn/poker/model/HandTest.java
+++ src/test/java/wtf/sinn/poker/model/HandTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class HandTest {
     @Nested
@@ -19,6 +20,28 @@ public class HandTest {
                     new Card(CardSuit.HEARTS, CardValue.FIVE),
                     new Card(CardSuit.HEARTS, CardValue.SIX)
             ))).doesNotThrowAnyException();
+        }
+
+        @Test
+        void should_detect_that_hand_has_less_than_5_cards() {
+            assertThatThrownBy(() -> new Hand(List.of(
+                    new Card(CardSuit.HEARTS, CardValue.TWO),
+                    new Card(CardSuit.HEARTS, CardValue.THREE),
+                    new Card(CardSuit.HEARTS, CardValue.FOUR),
+                    new Card(CardSuit.HEARTS, CardValue.FIVE)
+            ))).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void should_detect_that_hand_has_more_than_5_cards() {
+            assertThatThrownBy(() -> new Hand(List.of(
+                    new Card(CardSuit.HEARTS, CardValue.TWO),
+                    new Card(CardSuit.HEARTS, CardValue.THREE),
+                    new Card(CardSuit.HEARTS, CardValue.FOUR),
+                    new Card(CardSuit.HEARTS, CardValue.FIVE),
+                    new Card(CardSuit.HEARTS, CardValue.SIX),
+                    new Card(CardSuit.HEARTS, CardValue.SEVEN)
+            ))).isInstanceOf(IllegalArgumentException.class);
         }
     }
 }
